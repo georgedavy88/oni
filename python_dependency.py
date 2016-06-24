4e59b544823c653b059a4d10f31800fcc3ebe04e
George Davy <georgedavy@live.com>
2016-06-23 17:41:15 -0700

    Create python_dependency.py

---
+++ python_dependency.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ONI-python',
+      description='ONI python dependencies',
+      install_requires=['lxml','scrapy','sh','wheel','watchdog','pika'],
+     )
